13d70cb4d8bb469841f89cd42f59c427185c6ba0
nadhiffuadi <nadhiffuadi@gmail.com>
2025-02-06 11:12:22 +0700

    initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,21 @@
+import { User } from "@/types/user";
+
+export default async function HomePage() {
+  const response = await fetch("http://localhost:8000/api/v1/users");
+  const users = await response.json();
+
+  console.log(users);
+
+  return (
+    <section>
+      {users.data.map((user: User) => {
+        return (
+          <div key={user.id} className="mb-5">
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
